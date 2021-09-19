eecab9f91ac8b640dc3ece988dfde5a04feee7d8
Liz Wilson <lizriderwilson@gmail.com>
2021-09-19 16:27:44 -0400

    Update session_store.rb to work in development and production

--- backend/config/initializers/session_store.rb
+++ backend/config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_minimum_entry", domain: "localhost:3000"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_minimum_entry", domain: "minimum-entry.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_minimum_entry"
+end
