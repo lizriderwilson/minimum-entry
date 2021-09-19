35c4db84651d30fd6fd459f69f5041e878013e91
Liz Wilson <lizriderwilson@gmail.com>
2021-09-19 17:05:47 -0400

    Set up initial Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Starter App</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={'/'} component={Home}/>
+            <Route exact path={'/dashboard'} component={Dashboard}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
