6603a1f9b388e1447f07279782297d3af3b285b0
Liz Wilson <lizriderwilson@gmail.com>
2021-09-19 16:19:22 -0400

    Finish API authentication

--- backend/app/controllers/api/v1/sessions_controller.rb
+++ backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+  include CurrentUserConcern
 
   def create
     user = User.find_by(username: params[:username])
@@ -14,6 +15,27 @@ class Api::V1::SessionsController < ApplicationController
         status: 401
       }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :create]
       resources :sessions, only: [:create]
+      delete '/logout', to: "sessions#logout"
+      get '/logged_in', to: "sessions#logged_in"
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
